ed6c747ed6596efc178ebb8c1f7a11cb9c69c195
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-03-21 10:13:26 -0400

    Create Survey Changes
    Create Survey Changes

--- src/app/pages/survey/survey.module.ts
+++ src/app/pages/survey/survey.module.ts
@@ -7,8 +7,20 @@ import { ListSurveyComponent } from '../survey/list-survey/list-survey.component
 import { AddSurveyComponent } from './add-survey/add-survey.component';
 import { EditSurveyComponent } from './edit-survey/edit-survey.component';
 
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule, } from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule],
+  imports: [BrowserModule, FormsModule, RouterModule,
+    MatFormFieldModule, MatSelectModule, MatInputModule, BrowserAnimationsModule, MatRadioModule,
+    MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatTableModule],
   declarations: [ListSurveyComponent, AddSurveyComponent, EditSurveyComponent],
   exports: [ListSurveyComponent, AddSurveyComponent, EditSurveyComponent]
 })
