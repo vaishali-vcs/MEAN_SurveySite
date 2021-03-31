bdb5ceda2017df8d5f5fa7729f0ccaeb21940e78
SamridhiBindal06 <sbindal1@mycentennialcollege.ca>
2021-04-01 02:17:11 +0530

    Added Login View

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
+export class LoginComponent {
+  isLoading = false;
 
-export class LoginComponent {}
+  constructor() {}
+
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+
+  }
+}
